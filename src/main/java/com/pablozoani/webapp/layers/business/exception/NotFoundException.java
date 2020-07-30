791641fccfaa3eb6a5d69b19ffdff98c0977764a
Pablo Zoani <pablozoani@hotmail.com>
2020-07-30 14:32:59 -0300

    added 404 exception class

---
+++ src/main/java/com/pablozoani/webapp/layers/business/exception/NotFoundException.java
@@ -0,0 +1,8 @@
+package com.pablozoani.webapp.layers.business.exception;
+
+public class NotFoundException extends RuntimeException {
+
+    public NotFoundException(String message) {
+        super(message);
+    }
+}
